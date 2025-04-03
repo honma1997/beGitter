cd90f2771a7901a8b0e9728661556196c6e18450
EC2 Default User <ec2-user@ip-172-31-9-38.ap-northeast-1.compute.internal>
2025-04-03 17:48:06 +0000

    ルーティングまで

---
+++ app/controllers/public/users_controller.rb
@@ -0,0 +1,22 @@
+class Public::UsersController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def mypage
+  end
+
+  def index
+  end
+
+  def search
+  end
+
+  def destroy
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,30 @@
+Rails.application.routes.draw do
+  # Devise
+  devise_for :users
+
+  # ルートページ
+  root to: "public/homes#top"
+
+  # 一般ユーザー用
+  scope module: :public do
+    get 'mypage', to: 'users#mypage'
+    resources :users do
+      resource :relationship, only: [:create, :destroy]
+      collection do
+        get :search
+      end
+    end
+    resources :posts do
+      resource :like, only: [:create, :destroy]
+      resources :comments, only: [:create, :destroy]
+    end
+    resources :tags, only: [:index, :show]
+  end
+
+  # 管理者用
+  namespace :admin do
+    resources :users, only: [:index, :destroy]
+    resources :posts, only: [:index, :destroy]
+    resources :tags
+  end
+end
