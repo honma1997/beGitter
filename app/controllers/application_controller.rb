108047941c63340bb49c215a3f173957b3f30b45
EC2 Default User <ec2-user@ip-172-31-9-38.ap-northeast-1.compute.internal>
2025-04-07 16:48:12 +0000

    ヘッダー修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
 class ApplicationController < ActionController::Base
-   # 管理者 or ユーザーのログイン後のリダイレクト先を分岐
-   def after_sign_in_path_for(resource)
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  # 既存のafter_sign_in_path_forメソッドはそのまま維持
+  def after_sign_in_path_for(resource)
     if resource.is_a?(Admin)
       admin_users_path  # 管理者用ダッシュボードや一覧など好きな場所に変更してOK
     else
       root_path  # 一般ユーザーのトップページ
     end
   end
+  
+  protected
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :phase])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :phase])
+  end
 end
-
