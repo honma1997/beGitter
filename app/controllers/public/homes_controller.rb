a44b1abb5d139787fd83d8c33e9497a87708031a
EC2 Default User <ec2-user@ip-172-31-9-38.ap-northeast-1.compute.internal>
2025-04-11 05:32:36 +0000

    フェーズ1

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,4 +1,7 @@
+# app/controllers/public/homes_controller.rb
 class Public::HomesController < ApplicationController
   def top
+    @posts = Post.includes(:user, :tags).order(created_at: :desc).limit(5)
+    @users = User.order(created_at: :desc).limit(5)
   end
 end
--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,9 +1,19 @@
-# app/controllers/public/users_controller.rb
 class Public::UsersController < ApplicationController
-  before_action :authenticate_user!
-  before_action :set_user, only: [:edit, :update]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_user, only: [:show, :edit, :update]
   before_action :ensure_correct_user, only: [:edit, :update]
 
+  # ユーザー一覧
+  def index
+    @users = User.page(params[:page]).per(12)
+  end
+
+  # ユーザー詳細
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts.order(created_at: :desc).page(params[:page]).per(5)
+  end
+  
   # マイページ
   def mypage
     @user = current_user
@@ -21,6 +31,12 @@ class Public::UsersController < ApplicationController
     else
       render :edit
     end
+  end
+
+  # ユーザー検索
+  def search
+    @users = User.where("name LIKE ?", "%#{params[:keyword]}%").page(params[:page]).per(12)
+    render :index
   end
 
   private
