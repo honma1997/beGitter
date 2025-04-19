b6d4d0495e0145ad1c046cfe834c8bf1000ca2c4
EC2 Default User <ec2-user@ip-172-31-9-38.ap-northeast-1.compute.internal>
2025-04-19 00:28:00 +0000

    コメント追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,20 @@ Admin.find_or_create_by!(email: "admin@gmail.com") do |admin|
   admin.password = "123456"
   admin.password_confirmation = "123456"
 end
+
+# ユーザー5人作成
+5.times do |i|
+  user = User.create!(
+    email: "user#{i + 1}@example.com",
+    password: "password",
+    name: "ユーザー#{i + 1}"
+  )
+
+  # 各ユーザーに3つの投稿を作成
+  3.times do |j|
+    user.posts.create!(
+      title: "ユーザー#{i + 1}の投稿#{j + 1}",
+      body: "これはユーザー#{i + 1}の#{j + 1}番目の投稿です。"
+    )
+  end
+end
